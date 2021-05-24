928cd5dcae674d8375f1a61f9fc082c0adecc748
nabomandy <andyairis@gmail.com>
2021-05-24 22:49:02 +0900

    JPA_Basic

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,22 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.lang.reflect.Member;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+
+
+    }
+}
